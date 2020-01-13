bd627afcc28c8555645bdb6f8febb2c5e44aaba8
Pyython <vogonpoetry06@gmail.com>
2020-01-13 17:02:27 +0700

    new approach for getting balances from upgrade calls

--- src/mappings/converterUpgrader.ts
+++ src/mappings/converterUpgrader.ts
@@ -4,6 +4,12 @@ import {
     UpgradeOldCall
 } from "../../generated/ConverterUpgraderContract/ConverterUpgraderContract"
 import {
+    ConverterContract
+} from "../../generated/ConverterUpgraderContract/ConverterContract"
+import {
+    ERC20Contract
+} from "../../generated/ConverterUpgraderContract/ERC20Contract"
+import {
     Converter,
     ConverterTokenBalance
 } from "../../generated/schema"
@@ -13,29 +19,47 @@ import {
 export function handleConverterUpgrade(event: ConverterUpgrade): void {
     let oldConverterAddress = event.params._oldConverter;
     let newConverterAddress = event.params._newConverter;
+    let oldConverterContract = ConverterContract.bind(oldConverterAddress);
+
 
     let oldConverterEntity = Converter.load(oldConverterAddress.toHex());
-
+    
     let newConverterEntity = Converter.load(newConverterAddress.toHex());
     if(newConverterEntity == null) {
         newConverterEntity = new Converter(newConverterAddress.toHex());
         newConverterEntity.smartToken = oldConverterEntity.smartToken;
         newConverterEntity.save();
     }
-    let oldConverterTokenBalances = oldConverterEntity.tokenBalances;
-    log.debug("Converter Upgrade fired: {} > {}, Old Converter Balances: {}", [oldConverterAddress.toHex(), newConverterAddress.toHex(), oldConverterTokenBalances.toString()]);
-    for(var i = 0; i < oldConverterTokenBalances.length; i++) {
-        let oldConverterTokenBalanceID = oldConverterTokenBalances[i];
+    let connectorCount = oldConverterContract.connectorTokenCount();
+    for(var i = 0; i < connectorCount; i++) {
+        let tokenAddress = oldConverterContract.connectorTokens(BigInt.fromI32(i));
+        let oldConverterTokenBalanceID = oldConverterAddress.toHex() + "-" + tokenAddress.toHex();
         let oldConverterTokenBalanceEntity = new ConverterTokenBalance(oldConverterTokenBalanceID);
-        let newConverterTokenBalanceID = newConverterAddress.toHex() + "-" + oldConverterTokenBalanceEntity.token;
-        let newConverterTokenBalanceEntity = new ConverterTokenBalance(newConverterTokenBalanceID);
-        newConverterTokenBalanceEntity.token = oldConverterTokenBalanceEntity.token;
-        newConverterTokenBalanceEntity.converter = newConverterAddress.toHex();
-        newConverterTokenBalanceEntity.balance = oldConverterTokenBalanceEntity.balance;
         oldConverterTokenBalanceEntity.balance = BigInt.fromI32(0);
+        let newConverterTokenBalanceID = newConverterAddress.toHex() + "-" + tokenAddress.toHex();
+        let newConverterTokenBalanceEntity = new ConverterTokenBalance(newConverterTokenBalanceID);
+        newConverterTokenBalanceEntity.token = tokenAddress.toHex();
+        newConverterTokenBalanceEntity.converter = newConverterAddress.toHex();
+        let tokenContract = ERC20Contract.bind(tokenAddress);
+        newConverterTokenBalanceEntity.balance = tokenContract.balanceOf(newConverterAddress);
         newConverterTokenBalanceEntity.save();
         oldConverterTokenBalanceEntity.save();
     }
+    // let oldConverterTokenBalances = oldConverterEntity.tokenBalances as string[];
+    // log.debug("Converter Upgrade fired: {} > {}, Old Converter Balances: {}", [oldConverterAddress.toHex(), newConverterAddress.toHex(), oldConverterTokenBalances.toString()]);
+    // for(var i = 0; i < oldConverterTokenBalances.length; i++) {
+    //     let oldConverterTokenBalanceID = oldConverterTokenBalances[i];
+    //     log.debug("Loading Converter balances ({}): {}", [oldConverterAddress.toHex(), oldConverterTokenBalanceID]);
+    //     let oldConverterTokenBalanceEntity = new ConverterTokenBalance(oldConverterTokenBalanceID);
+    //     let newConverterTokenBalanceID = newConverterAddress.toHex() + "-" + oldConverterTokenBalanceEntity.token;
+    //     let newConverterTokenBalanceEntity = new ConverterTokenBalance(newConverterTokenBalanceID);
+    //     newConverterTokenBalanceEntity.token = oldConverterTokenBalanceEntity.token;
+    //     newConverterTokenBalanceEntity.converter = newConverterAddress.toHex();
+    //     newConverterTokenBalanceEntity.balance = oldConverterTokenBalanceEntity.balance;
+    //     oldConverterTokenBalanceEntity.balance = BigInt.fromI32(0);
+    //     newConverterTokenBalanceEntity.save();
+    //     oldConverterTokenBalanceEntity.save();
+    // }
 }
 
 export function handleUpgradeOld(call: UpgradeOldCall): void {
